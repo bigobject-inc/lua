3d2fc3d5ef074dc8ed43388e058004904a4d5ddd
yceugenelai <eugene@macrodatalab.com>
2015-05-15 11:01:27 +0800

    Create AMinusB.lua

---
+++ AMinusB.lua
@@ -0,0 +1,18 @@
+function AMinusB(a , attr_a , attr_b)
+	sv = a:getChildrenIdValue(1);
+	a_v = 0
+	b_v = 0
+	for id,v in pairs(sv) do
+		--print (id , v)
+		if id == attr_a then
+			a_v = v
+		end
+		if id == attr_b then
+			b_v = v
+		end	
+	end
+	value = (a_v - b_v)
+	a:assign(0 , value)
+	
+	return true
+end
