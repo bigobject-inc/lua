6314d3cd8cd3a415147f8fd94ccb402e17975432
yceugenelai <eugene@bigobject.io>
2017-01-04 12:28:38 +0800

    Update quantile.lua

--- quantile.lua
+++ quantile.lua
@@ -1,4 +1,7 @@
 function quantile(a, percentage)
+        if tonumber(percentage) < 0 or tonumber(percentage) > 1 then
+                error("quantile argument should be > 0 and <= 1")
+	end
         data_array = a:getChildrenValue(1);
         table.sort(data_array)
         index = math.floor((#data_array)*percentage + 0.5)
